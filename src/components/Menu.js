81105db3132700298e76f74b1480bf15fe8d4838
Peter Witham <peter.witham@gmail.com>
2019-08-25 15:46:03 -0500

    Added menu link to the mailing list sign up page.

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -46,7 +46,16 @@ function Menu() {
           <p>{m.name}</p>
         </Link>
       ))}
-    </div>
+      <a style={{
+        boxShadow: `none`,
+        textDecoration: `none`,
+        textTransform: "uppercase",
+        letterSpacing: 1,
+        color: "white",
+        ...scale(0 / 5),
+        marginRight: 20,
+      }} href={"https://mailchi.mp/9e0f70c40b39/cswift"}> MAILING LIST</a>
+        </div>
   )
 }
 
