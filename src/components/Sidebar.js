34a63f80f16843d669a7c7e278287dc5e23d6baf
Peter Witham <peter.witham@gmail.com>
2020-07-02 12:33:15 -0500

    Added alt to image tag

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -6,7 +6,11 @@ function sidebar() {
       style={{ display: "flex", justifyContent: "center", marginBottom: 30 }}
     >
       <a href={"https://patreon.com/pwcom"}>
-        <img src={"/images/cs_patreon.png"} style={{ maxWidth: "100%" }} />
+        <img
+          src={"/images/cs_patreon.png"}
+          style={{ maxWidth: "100%" }}
+          alt="Subscribe to site"
+        />
       </a>
     </div>
   )
