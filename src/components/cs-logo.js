aa0bcdb667552948f0ea54891287006b0e93a398
Peter Witham <peter.witham@gmail.com>
2020-09-21 16:23:46 -0500

    Updated logo for 2020

--- src/components/cs-logo.js
+++ src/components/cs-logo.js
@@ -1,21 +1,16 @@
-import React from "react"
+import React from 'react';
 
 function CSLogo({ data }) {
-  return (
-    <div
-      style={{
-        display: "flex",
-        alignItems: "center",
-      }}
-    >
-      <img
-        src={require("../assets/cs-logo-square-512.png")}
-        width="50"
-        height="50"
-        alt="CompileSwift Logo"
-      />
-    </div>
-  )
+	return (
+		<div
+			style={{
+				display: 'flex',
+				alignItems: 'center',
+			}}
+		>
+			<img src={require('../assets/cs-logo-2020.png')} alt="CompileSwift Logo" />
+		</div>
+	);
 }
 
-export default CSLogo
+export default CSLogo;
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,78 +1,69 @@
-import React from "react"
-import { Link } from "gatsby"
-import Headroom from "react-headroom"
-import { scale } from "../utils/typography"
-import CSLogo from "./cs-logo"
+import React from 'react';
+import { Link } from 'gatsby';
+import Headroom from 'react-headroom';
+import { scale } from '../utils/typography';
+import CSLogo from './cs-logo';
 
-import Menu from "./Menu"
-import SocialLinks from "../components/SocialLinks"
+import Menu from './Menu';
+import SocialLinks from '../components/SocialLinks';
 
-import "../assets/darkmode.css"
+import '../assets/darkmode.css';
 
 class Layout extends React.Component {
-  render() {
-    const { title, children } = this.props
-    let header
+	render() {
+		const { title, children } = this.props;
+		let header;
 
-    header = (
-      <div
-        style={{
-          backgroundColor: "#f57738",
-          paddingTop: 40,
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          flexDirection: "column",
-        }}
-      >
-        <Link
-          style={{
-            boxShadow: `none`,
-            textDecoration: `none`,
-            color: `white`,
-            display: "flex",
-            justifyContent: "center",
-          }}
-          to={`/`}
-        >
-          <CSLogo />
-          <h1
-            style={{
-              ...scale(2 / 2.25),
-              fontWeight: "bold",
-              marginLeft: 20,
-              marginTop: 5,
-            }}
-          >
-            {title}
-          </h1>
-        </Link>
-        <br />
-        <SocialLinks />
-        <Menu />
-      </div>
-    )
+		header = (
+			<div
+				style={{
+					backgroundColor: '#f57738',
+					paddingTop: 40,
+					display: 'flex',
+					justifyContent: 'center',
+					alignItems: 'center',
+					flexDirection: 'column',
+					height: 230,
+				}}
+			>
+				<Link
+					style={{
+						boxShadow: `none`,
+						textDecoration: `none`,
+						color: `white`,
+						display: 'flex',
+						justifyContent: 'center',
+					}}
+					to={`/`}
+				>
+					<CSLogo />
+				</Link>
+				<br />
+				<SocialLinks />
+				<Menu />
+			</div>
+		);
 
-    return (
-      <div
-        style={{
-          marginLeft: `auto`,
-          marginRight: `auto`,
-        }}
-      >
-        <Headroom>
-          <header>{header}</header>
-        </Headroom>
+		return (
+			<div
+				style={{
+					marginLeft: `auto`,
+					marginRight: `auto`,
+				}}
+			>
+				<Headroom>
+					<header>{header}</header>
+				</Headroom>
 
-        <main>{children}</main>
-        {/* <footer>
+				<main>{children}</main>
+				{/* <footer>
           © {new Date().getFullYear()}, Built with
           {` `}
           <a href="https://www.gatsbyjs.org">Gatsby</a>
         </footer> */}
-      </div>
-    )
-  }
+			</div>
+		);
+	}
 }
 
-export default Layout
+export default Layout;
