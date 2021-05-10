8acda22810c956e771059e9a728e18e2ed1cf12e
Peter Witham <peter.witham@gmail.com>
2021-05-10 14:36:48 -0500

    Added pocast links to navigation

--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -1,15 +1,37 @@
 import React from "react"
 
-function socialLinks (){
+function socialLinks() {
   return (
-    <div style={{
-      display: 'flex',
-      flexDirection: 'row',
-      color: 'white'
-    }}>
-      <a style={{color: 'white'}} href={"https://twitter.com/compileswift"}><p>Twitter &nbsp;&nbsp;</p></a>
-      <a style={{color: 'white'}} href={"https://instagram.com/compileswift"}><p>Instagram &nbsp;&nbsp;</p></a>
-      <a style={{color: 'white'}} href={"https://patreon.com/compileswift"}><p>Patreon &nbsp;&nbsp;</p></a>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        color: "white",
+      }}
+    >
+      <a style={{ color: "white" }} href={"https://twitter.com/compileswift"}>
+        <p>Twitter &nbsp;&nbsp;</p>
+      </a>
+      <a style={{ color: "white" }} href={"https://instagram.com/compileswift"}>
+        <p>Instagram &nbsp;&nbsp;</p>
+      </a>
+      <a style={{ color: "white" }} href={"https://patreon.com/compileswift"}>
+        <p>Patreon &nbsp;&nbsp;</p>
+      </a>
+      <a
+        style={{ color: "white" }}
+        href={
+          "https://podcasts.apple.com/us/podcast/compile-swift/id1467065787"
+        }
+      >
+        <p>Listen on Apple Podcasts &nbsp;&nbsp;</p>
+      </a>
+      <a
+        style={{ color: "white" }}
+        href={"https://open.spotify.com/show/2RGHkl4UtYj0V7lPcRojzK"}
+      >
+        <p>Listen on Spotify &nbsp;&nbsp;</p>
+      </a>
     </div>
   )
 }
