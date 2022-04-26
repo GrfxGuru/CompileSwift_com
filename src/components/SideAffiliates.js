24a013b0b5b9ab84e6781a235c92cc7e3feb1c5f
Peter Witham <peter.witham@gmail.com>
2022-04-26 04:48:17 +0000

    Added Swift Remote Studio to sidebar.

--- src/components/SideAffiliates.js
+++ src/components/SideAffiliates.js
@@ -1,18 +1,22 @@
-import React from 'react';
-import Afso from '../assets/aff-sa.png';
-import Afdo from '../assets/aff-do.png';
+import React from "react"
+import Afso from "../assets/aff-sa.png"
+import Afdo from "../assets/aff-do.png"
+import Afsrs from "../assets/swift-remote-studio.jpg"
 
 const SideAffiliates = ({ source, alt }) => {
-	return (
-		<div>
-			<a href="https://pw.d.pr/7f31K6">
-				<img src={Afso} alt={"SetApp"}/>
-			</a>
-			<a href="https://pw.d.pr/kOYVpQ">
-				<img src={Afdo} alt={"DigitalOcean"}/>
-		</a>
-	</div>
-	)
-};
+  return (
+    <div>
+      <a href="https://swiftremotestudio.com" target="_blank">
+        <img src={Afsrs} alt={"Swift Remote Studio"} />
+      </a>
+      <a href="https://pw.d.pr/7f31K6" target="_blank">
+        <img src={Afso} alt={"SetApp"} />
+      </a>
+      <a href="https://pw.d.pr/kOYVpQ" target="_blank">
+        <img src={Afdo} alt={"DigitalOcean"} />
+      </a>
+    </div>
+  )
+}
 
-export default SideAffiliates;
+export default SideAffiliates
